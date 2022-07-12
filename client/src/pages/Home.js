9df557fb2cf3f7d1f17585f52d9d77ec9a0b096d
Isaiah Evensvold <38709459+IsaiahEvensvold@users.noreply.github.com>
2022-07-12 00:26:10 -0400

    setup

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className='title'>
+          Hey Peter
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
